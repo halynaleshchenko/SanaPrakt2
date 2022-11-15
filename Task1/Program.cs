7811bbdf72f8c09cc7cffd951e51b46068bd03e3
halynaleshchenko <ipz222_lgo@student.ztu.edu.ua>
2022-11-15 12:50:06 +0200

    new line 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("new line1");
 Console.WriteLine("Sana Commerce Software De");
-Console.WriteLine("Commerce Software Development Boo");
+Console.WriteLine("Курси SCommerce Software Development Boo");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Develop]]]ment Bootcamp");
+Console.WriteLine("Курси Sana Commerce Sofware Develont Bootcamp");
